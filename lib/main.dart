84a002fa64dc9498baa2e9fc8ed9e504f389bd4c
olaabdul <52639319+olaabdul@users.noreply.github.com>
2020-11-10 01:40:29 +0100

    I have updated the app features including my details. Also, learn along during the process

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,95 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyAppp());
+}
+
+class MyAppp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 70.0,
+                backgroundImage: AssetImage('images/abdul.jpg'),
+              ),
+              Text(
+                'Abdul Ola',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'DESIGNER AND FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal[100],
+                    letterSpacing: 3.0),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 25.0),
+                color: Colors.white,
+                child: Padding(
+                    padding: EdgeInsets.all(25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'abdulyoung78@gmail.com',
+                        style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal[900],
+                        ),
+                      ),
+                    )),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 25.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+234-909-585-5552',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.teal[900],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
